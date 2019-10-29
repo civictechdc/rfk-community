1b9dd259f635b3594c47d354ef2113417ef9b93a
Martin Dertz <martydertz@gmail.com>
2019-10-28 21:17:21 -0400

    added postgresql db and user model

--- django-project/rfk_community/rfk_community/settings.py
+++ django-project/rfk_community/rfk_community/settings.py
@@ -74,12 +74,18 @@ WSGI_APPLICATION = 'rfk_community.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rfk_community',
+        'USER': 'SAMPLE_USER',
+        'PASSWORD':'SAMPLE_PASSWORD',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
+
 
 
 # Password validation
