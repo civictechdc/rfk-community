06da5bb935b314ff4304908632fb11e4dce3a111
Hayden Betts <haydenbetts@gmail.com>
2020-01-29 20:07:09 -0500

    Who made this survey copy

--- django-project/rfk_community_react/src/routes/who-are-we/index.js
+++ django-project/rfk_community_react/src/routes/who-are-we/index.js
@@ -35,7 +35,9 @@ const WhoAreWe = ({ classes }) => {
               <Box mb={1} style={{ cursor: 'pointer' }} onClick={() => window.open('https://codefordc.org')}>
                 <img src={Logo} style={{ width: 50 }} />
               </Box>
-              This website is a project of volunteers with Code for DC and the office of Ward 6 Councilmember Charles Allen. It is our goal to open up the conversation around the future of the RFK campus and give DC residents a voice in what the space could be used for, including the possibility of an NFL stadium, but without limiting the opportunity to build houses, parks, or more. We decided to build a survey tool to help kickstart the community engagement and conversation.
+              <Box style={{ textAlign: 'left' }}>
+                <p>This website is a project of volunteers with Code for DC and the office of Ward 6 Councilmember Charles Allen. </p> <p>It is our goal to open up the conversation around the future of the RFK campus and give DC residents a voice in what the space could be used for, including the possibility of an NFL stadium, but without limiting the opportunity to build houses, parks, or more. </p> We decided to build a survey tool to help kickstart the community engagement and conversation.
+              </Box>
             </CardContent>
           </Card>
         </Grid>
